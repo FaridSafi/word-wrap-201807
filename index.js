a8f01556ba8f862b184be216cee06606ab7d3310
Farid Safi <faridsafi@users.noreply.github.com>
2018-07-04 10:50:24 +0200

    Add default route

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.get("/", function(req, res) {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started");
+});
